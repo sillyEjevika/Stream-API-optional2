884afcb5b3a42bd5be281a0b7568bbf0b2af66de
sillyEjevika <Doroteia_Vika@mail.ru>
2023-03-14 11:05:21 +0300

    Задача №1,2

--- Stream-Api-and-Optional-master/src/main/java/ru/sillyEjevika/Main.java
+++ Stream-Api-and-Optional-master/src/main/java/ru/sillyEjevika/Main.java
@@ -7,17 +7,17 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        // Задание №1
+        // Задача №1 (ч1)
         List<Integer> integers = List.of(1, 2, 4, 5, 6, 9, 3, 10, 7, 8);
         List<String> strings = List.of("a", "b", "f", "g", "c", "d");
         findMinMax(integers.stream(), Integer::compareTo, (x, y) -> System.out.println("Минимальное число: " + x + "\nМаксимальное число: " + y));
         findMinMax(strings.stream(), String::compareTo, (x, y) -> System.out.println("Первая буква: " + x + "\nПоследняя буква: " + y));
 
-        // ч2
+        // Задача №1 (ч2)
         printNumberOfEven(integers);
     }
 
-    // Задание №1
+    // Задача №1
     private static <T> void findMinMax(Stream<? extends T> stream, Comparator<? super T> order, BiConsumer<? super T, ? super T> minMaxConsumer) {
         List<? extends T> list = stream.sorted(order).toList();
         if (list.isEmpty()) {
@@ -28,7 +28,7 @@ public class Main {
 
     }
 
-    // Задание №2
+    // Задача №2
     private static void printNumberOfEven(List<Integer> integers){
         System.out.println("Кол-во чётных чисел: " + integers.stream()
                 .filter(x -> x % 2 == 0)
